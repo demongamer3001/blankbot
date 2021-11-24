f12d1f33c79eedc3121b69f931bfed40b2aa7459
Blank-c <94945186+Blank-c@users.noreply.github.com>
2021-11-24 22:08:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-#pylint:disable=E1102
 from termcolor import colored
 from datetime import datetime
 dt = datetime.now()
@@ -61,9 +60,9 @@ async def change_activity():
     elif activity=="p":
         activity=discord.Game(name=random.choice(activity_p))
     elif activity=="w":
-        activity=discord.Activity(type=discord.ActivityType.watching, name=random.choice(activity_w)))
+        activity=discord.Activity(type=discord.ActivityType.watching, name=random.choice(activity_w))
     else:
-        activity=discord.Activity(type=discord.ActivityType.listening, name=random.choice(activity_l)))
+        activity=discord.Activity(type=discord.ActivityType.listening, name=random.choice(activity_l))
     await Blank.change_presence(activity=activity)
 
 @Blank.event
